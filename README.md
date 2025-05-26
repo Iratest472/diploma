eb29f974133a933b3ccc570ba43a196f38d250a2
Iratest472 <kurs30895@gmail.com>
2025-05-26 17:09:53 +0300

    готово

--- README.md
+++ README.md
@@ -4,7 +4,29 @@
 - pytest 
 - selenium 
 - request 
+- allure
+- config
+
+### Структура
+- ./test - тесты
+- ./pages - описание страниц
+- ./api - хелперы для работы с API
+
+### Шаги
+1. Склонировать проект 'https://github.com/Iratest472/diploma.git'
+2. Установить зависимости
+3. Запустить тесты 'pytest'
+4. Сгенерировать отчет 'allure generate allure-files -o allure-report'
+5. Открыть отчет 'allure open allure-report'
 
 ### Полезные ссылки
 - [Подсказка по markdown](https://www.markdownguide.org/basic-syntax/)
 - [Генератор файла .gitignore]( https://www.toptal.com/developers/gitignore/)
+
+### Библиотеки
+
+- pip3 install pytest
+- pip install selenium
+- pip install webdriver-manager
+- pip3 install requests
+- pip install allure-pytest
---
+++ pages/api_page.py
@@ -0,0 +1,37 @@
+import requests
+import allure
+from selenium.webdriver.common.by import By
+
+class KinoApi:
+    def __init__(self, api_url: str, my_headers) -> None:
+        self.api_url = api_url
+        self.my_headers = my_headers
+
+
+    @allure.step("Получаем названия фильмов")
+    def search_for_movie_name(self, movie_name: str):
+        my_params = {
+            'page': 1,
+            'limit': 1,
+            'query': movie_name}
+        resp = requests.get(self.api_url + 'v1.4/movie/search', params=my_params, headers=self.my_headers)
+        return resp
+
+    def search_for_movie_id(self, movie_id: str):
+        my_params = {
+        }
+        resp = requests.get(self.api_url + f'v1.4/movie/{movie_id}', params=my_params, headers=self.my_headers)
+        return resp
+
+    def search_for_actors_name(self, actors_name: str):
+        my_params = {
+            'page': 1,
+            'limit': 1,
+            'query': actors_name}
+        resp = requests.get(self.api_url + 'v1.4/person/search', params=my_params, headers=self.my_headers)
+        return resp
+
+    @allure.step("Поиск контента по фразе: {phrase}")
+    def search_items_by_phrase(self, phrase):
+        self._wait_for_elements(By.CSS_SELECTOR, "input[name=kp_query]").send_keys(phrase)
+        self.driver.find_element(By.CSS_SELECTOR, "button[type=submit]").click()
