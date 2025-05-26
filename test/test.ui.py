595dc1126ef364ced9376c37e6d7142437e82486
Iratest472 <kurs30895@gmail.com>
2025-05-26 17:37:42 +0300

    готово

---
+++ test/test.ui.py
@@ -0,0 +1,100 @@
+import pytest
+import allure
+from selenium import webdriver
+from pages.main_page import MainPage
+from config import main_url, cookies
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture
+def main_page(driver):
+    driver.get(main_url)
+
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+
+    driver.get(main_url)
+    return MainPage(driver, main_url)
+
+
+@allure.feature("Smoke")
+@allure.story("UI")
+@allure.title("Проверка заголовка главной страницы")
+@pytest.mark.smoke
+def test_check_main_page_title(main_page):
+    with allure.step("Заголовок главной страницы"):
+        assert main_page.check_page_title(
+            "Кинопоиск. Онлайн кинотеатр. Фильмы сериалы мультфильмы и энциклопедия")
+
+@allure.feature("Поиск")
+@allure.story("UI")
+@allure.title("Поиск фильмов по названию")
+@pytest.mark.parametrize("film_title",
+                         ["Аватар", "Такси 4"])
+def test_search_book_by_title(main_page, film_title):
+    with allure.step(f"Поиск фильмов по названию"
+                     f" {film_title}"):
+        main_page.search_items_by_phrase(film_title)
+    with allure.step("Количество результатов больше 0"):
+        assert main_page.get_search_results_count() > 0
+    with allure.step(f"Название фильма содержится в результатах"):
+        assert film_title in main_page.find_book_titles()
+
+@allure.feature("Поиск")
+@allure.story("UI")
+@allure.title("Поиск персонажей по имени")
+@pytest.mark.parametrize("person_name",
+                         ["Леонардо Ди Каприо", "Квентин Тарантино"])
+def test_search_person_by_name(main_page, person_name):
+    """ Тестируем возможность поиска актеров, режиссеров и др. персонажи по имени. """
+    with allure.step(f"Ищем персонаж по имени {person_name}"):
+        main_page.search_items_by_phrase(person_name)
+
+    with allure.step("Убедимся, что количество результатов поиска больше нуля"):
+        assert main_page.get_search_results_count() > 0, \
+               f"Ничего не найдено по запросу '{person_name}'. Ожидаем минимум один результат."
+
+    with allure.step(f"Убеждаемся, что имя {person_name} встречается в списке результатов"):
+        results_list = main_page.find_person_names()
+        assert any(name.lower() == person_name.lower() for name in results_list), \
+               f"Имя '{person_name}' не обнаружено в результатах поиска."
+
+@allure.feature("Поиск")
+@allure.story("UI")
+@allure.title("Поиск фильмов по названию только дефисы")
+@pytest.mark.parametrize("film_title",
+                         ["-----", "-----"])
+def test_search_book_by_title_negative(main_page, film_title):
+    with allure.step(f"Поиск фильмов по названию"
+                     f" {film_title}"):
+        main_page.search_items_by_phrase(film_title)
+    with allure.step("Количество результатов больше 0"):
+        assert main_page.get_search_results_count() > 0
+    with allure.step(f"Название фильма содержится в результатах"):
+        assert film_title in main_page.find_book_titles()
+
+
+@allure.feature("Поиск")
+@allure.story("UI")
+@allure.title("Поиск персонажей по имени только пробелами")
+@pytest.mark.parametrize("person_name",
+                         ["       ", "       "])
+def test_search_person_by_name(main_page, person_name):
+    """ Тестируем возможность поиска актеров, режиссеров и др. персонажи по имени. """
+    with allure.step(f"Ищем персонаж по имени {person_name}"):
+        main_page.search_items_by_phrase(person_name)
+
+    with allure.step("Убедимся, что количество результатов поиска больше нуля"):
+        assert main_page.get_search_results_count() > 0, \
+               f"Ничего не найдено по запросу '{person_name}'. Ожидаем минимум один результат."
+
+    with allure.step(f"Убеждаемся, что имя {person_name} встречается в списке результатов"):
+        results_list = main_page.find_person_names()
+        assert any(name.lower() == person_name.lower() for name in results_list), \
+               f"Имя '{person_name}' не обнаружено в результатах поиска."
---
+++ test/test_api.py
@@ -0,0 +1,66 @@
+from config import  api_url, my_headers
+from pages.api_page import KinoApi
+import pytest
+import allure
+
+kino_api = KinoApi(api_url, my_headers)
+
+
+@allure.feature("Поиск")
+@allure.story("API")
+@allure.title("Поиск фильмов по названию")
+@pytest.mark.parametrize("film_title",
+                         ["Девчата", "Мамы"])
+def test_search_for_movie_by_name(film_title):
+    resp = kino_api.search_for_movie_name(film_title)
+    with allure.step(f"Поиск фильмов по названию {film_title}"):
+        data = resp.json()
+    with allure.step("Убедимся, что количество результатов поиска больше нуля"):
+        assert "docs" in data and len(data["docs"]) > 0
+        assert data["docs"][0]["name"] == film_title
+    with allure.step("Проверка статуса ответа"):
+        assert resp.status_code == 200
+
+@allure.feature("Поиск")
+@allure.story("API")
+@allure.title("Поиск фильмов по id")
+@pytest.mark.parametrize("film_id",
+                         ["555554", "254887"])
+def test_search_for_movie_by_id(film_id):
+    resp = kino_api.search_for_movie_id(film_id)
+    with allure.step(f"Поиск фильмов по id {film_id}"):
+        assert resp.json()["id"] == int(film_id)
+    with allure.step("Проверка статуса ответа"):
+        assert resp.status_code == 200
+
+@allure.feature("Поиск")
+@allure.story("API")
+@allure.title("Поиск актеров по имени")
+@pytest.mark.parametrize("name_actors",
+                         ["Ирина", "Денис"])
+def test_search_for_actors_by_name(name_actors):
+    resp = kino_api.search_for_actors_name(name_actors)
+    with allure.step(f"Поиск фильмов по id {name_actors}"):
+        assert resp.json()["docs"][0]["name"] == name_actors
+    with allure.step("Проверка статуса ответа"):
+        assert resp.status_code == 200
+
+@allure.feature("Поиск")
+@allure.story("API")
+@allure.title("Поиск актеров по имени только цифрами")
+@pytest.mark.parametrize("film_title", ["11111111", "1111111111"])
+def test_search_for_movie_by_name_negative(film_title):
+    resp = kino_api.search_for_movie_name(film_title)
+    with allure.step("Проверка статуса ответа"):
+        assert resp.status_code == 200
+
+@allure.feature("Поиск")
+@allure.story("API")
+@allure.title("Поиск актеров по имени только дефисами")
+@pytest.mark.parametrize("name_actors",
+                         ["--------", "-------"])
+def test_search_for_actors_by_name_negative(name_actors):
+    resp = kino_api.search_for_actors_name(name_actors)
+    with allure.step("Проверка статуса ответа"):
+        assert resp.status_code == 200
+
